fb908d0983d1169f0785667bfb1cac4ceef35fd0
matiascshaha <mp17m@my.fsu.edu>
2022-04-28 10:49:30 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import background from './abstract_blue_background.webp'
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage: 'URL(${background})' }}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{background-image: url('./abstract_blue_image.jpg')}}</div>
+    <p>matias</p>
   );
 }
 
